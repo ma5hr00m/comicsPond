2679abf752d63b3f070c76f7a07d8f47c7d53fd5
ma5hr00m <3411281455@qq.com>
2023-01-27 22:37:57 +0800

    update some scripts

---
+++ scripts/convertFiles.py
@@ -0,0 +1,5 @@
+'''
+Name: convertFiles.py
+
+Func: 将 comics.txt 转化为易于存储与记录的 comics.json .
+'''
---
+++ scripts/deduplicate.py
@@ -0,0 +1,7 @@
+'''
+Name: deduplicate.py
+
+Func: 遍历已有的 comicsDocs, 去除重复记录的漫画.
+
+Tips: 自时间最早的 comics.txt 开始, 若时间靠后的文件中出现重复的漫画, 则删除时间靠后的.
+'''
